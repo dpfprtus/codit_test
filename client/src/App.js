4e2b421c7c35a328bf984bad591fd293b21ee890
suyong choi <dpfprtus@gmail.com>
2024-10-06 20:04:53 +0900

    [FEAT] PDF upload to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useRef, useEffect } from "react";
-
 import * as pdfjsLib from "pdfjs-dist/webpack";
 import pdfjsWorker from "pdfjs-dist/build/pdf.worker.mjs.map";
 pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
 function App() {
   const [pdfFile, setPdfFile] = useState(null);
   const [pageNum, setPageNum] = useState(1);
@@ -43,13 +43,32 @@ function App() {
     });
   };
 
-  // PDF 파일 선택 핸들러 (Blob을 사용하여 파일 로드)
+  // PDF 파일 선택 핸들러 (Blob을 사용하여 파일 로드) => 파일선택 후 서버에 PDF 전송
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file && file.type === "application/pdf") {
       const pdfBlob = URL.createObjectURL(file);
       setPdfFile(pdfBlob);
+      handleFileUpload(file); // PDF 파일 서버에 전송
     }
+  };
+
+  // 서버로 PDF 파일 전송
+  const handleFileUpload = (file) => {
+    const formData = new FormData();
+    formData.append("pdf", file);
+
+    fetch("http://localhost:3000/api/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("파일 업로드 성공:", data);
+      })
+      .catch((error) => {
+        console.error("파일 업로드 실패:", error);
+      });
   };
 
   // 페이지 이동 핸들러
