05f16d20d40e6e3b1e3d082a719b19ed5031f77a
suyong choi <dpfprtus@gmail.com>
2024-10-06 20:31:03 +0900

    [FEAT] Cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import pdfRouter from "./src/api/pdf/pdf.index.js";
+import cors from "cors";
 const app = express();
 const port = 9000;
 
+app.use(cors());
 app.use("/api", pdfRouter);
 app.listen(port, () => {
   console.log(`${port}포트에서 서버가 열렸습니다.`);
